6a0349abd29db4827bfa4b7ab19ee2cab1c0baf7
CezarDev <bacanieskidev@gmail.com>
2024-08-06 16:49:41 -0300

    feat: criando carrinho reducer

--- src/context/CarrinhoContext.jsx
+++ src/context/CarrinhoContext.jsx
@@ -1,9 +1,12 @@
-import React, { createContext, useState } from 'react';
+import React, { createContext, useReducer, useState } from 'react';
 
 export const CarrinhoContext = createContext();
 CarrinhoContext.displayName = 'Carrinho';
+const estadoInicial = [];
+
+
 export const CarrinhoProvider = ({ children }) => {
-  const [carrinho, setCarrinho] = useState([]);
+  const [carrinho, dispatch] = useReducer(carrinhoReducer, estadoInicial);
   const [quantidade, setQuantidade] = useState(0);
   const [valorTotal, setValorTotal] = useState(0);
 
@@ -11,11 +14,9 @@ export const CarrinhoProvider = ({ children }) => {
     <CarrinhoContext.Provider
       value={{
         carrinho,
-        setCarrinho,
+        dispatch,
         quantidade,
-        setQuantidade,
         valorTotal,
-        setValorTotal,
       }}
     >
       {children}
